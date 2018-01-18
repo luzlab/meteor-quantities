e16f6f58f0b083ebc0e095751da5724abd4fe8d8
Carlo Quinonez <Carlo@studiofathom.com>
2018-01-17 18:34:23 -0800

    Added missing test deps

--- package.js
+++ package.js
@@ -21,5 +21,7 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('ecmascript');
+  api.use('ejson');
+  api.use('check');
   api.mainModule('test.js');
 });
